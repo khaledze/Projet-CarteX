fcdd5a1e546bd06b445a0644c0ef44db2c1a2b20
khaledze <k.aliahmed@ecole-ipssi.net>
2023-12-18 11:08:32 +0100

    fit

--- carte/src/components/Home.jsx
+++ carte/src/components/Home.jsx
@@ -1,21 +1,13 @@
 import React from 'react'
-
+import './Home.css'
+import Logo from '../img/Yugi.svg'
 export default function Home() {
   return (
     <div>      
-             
-
-
-
-
-
-
-
-
-
-
-
-
+      <header className="header">
+        <img src={Logo} alt="Logo" className="logo" />
+        <input type="text" className="search-bar" placeholder="Search..." />
+      </header>
     </div>
   )
 }
